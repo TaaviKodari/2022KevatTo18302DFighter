346fab2f153b9444e0701e47ba751c4effd6b594
kodarit11 <taavi.makela@gmail.com>
2022-02-17 19:29:27 +0200

    hyppy ja health aloitettu

---
+++ 2022TO18302DFighter/Assets/Scripts/Health.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public float maxhealth = 100;
+    public float health;
+    private float hitTimer = 0.15f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- 2022TO18302DFighter/Assets/Scripts/Movement.cs
+++ 2022TO18302DFighter/Assets/Scripts/Movement.cs
@@ -12,6 +12,7 @@ public class Movement : MonoBehaviour
 
     private float horizontalMovement = 0f;
     public int facing = 1;
+    public LayerMask layerMask;
     // Start is called before the first frame update
     void Start()
     {
@@ -22,6 +23,9 @@ public class Movement : MonoBehaviour
     void Update()
     {
         horizontalMovement = Input.GetAxis("Horizontal");
+        if(Input.GetButtonDown("Jump") && feet.IsTouchingLayers(layerMask)){
+            myRigidbody2D.AddForce(new Vector2(0f,jumpForce),ForceMode2D.Impulse);
+        }
     }
 
     void FixedUpdate()
