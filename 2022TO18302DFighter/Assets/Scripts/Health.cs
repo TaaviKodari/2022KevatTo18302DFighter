58d175c4da6fd5d8cdeb1e34264ee00d1ce53870
kodarit11 <taavi.makela@gmail.com>
2022-02-24 19:30:48 +0200

    health loppuun

--- 2022TO18302DFighter/Assets/Scripts/Health.cs
+++ 2022TO18302DFighter/Assets/Scripts/Health.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,16 +8,49 @@ public class Health : MonoBehaviour
     public float maxhealth = 100;
     public float health;
     private float hitTimer = 0.15f;
-    
+    public bool isHit = false;
+    public Rigidbody2D myRigidbody;
+    Movement moveScript;
+    private float damage;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        moveScript = GetComponent<Movement>();
+        health = maxhealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+       //Tee kuoleminen
+    }
+
+    public void TakeDamage(float damage)
+    {
+        if(!isHit)
+        {
+            health -= damage;
+            StartCoroutine(Knockback());
+        }
+    }
+
+    IEnumerator Knockback()
+    {
+        //Käskyt välittömästi
+        isHit = true;
+        myRigidbody.velocity = new Vector2(moveScript.facing * -2.5f, 2.5f);
+        //laskuri
+        yield return new WaitForSeconds(hitTimer);
+        //Ajastimen jälkeen tapahtuvat käskyt
+        isHit = false;
     }
 }
